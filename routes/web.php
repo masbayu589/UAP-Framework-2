64ce4e1ff34847fda17d1fc20808b56d86caff14
masbayu589 <masbayu589@gmail.com>
2023-11-10 14:52:16 +0700

    jawaban soal nomor 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::prefix('/mahasiswa')->group(function () {
+    Route::get('/pendaftaran', function () {
+        return 'Halaman pendaftaran untuk pendaftaran';
+    });
+
+    Route::get('/ujian', function () {
+        return 'Halaman Ujian untuk /ujian';
+    });
+
+    Route::get('/nilai', function () {
+        return 'Halaman nilai untuk /nilai';
+    });
+});
+
 Route::get('/', function () {
     return view('mahasiswa');
 });
